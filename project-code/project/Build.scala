b4682894b752221f249abc4f116d127ed145ff1a
Frogurth <artes.waldi@googlemail.com>
2012-05-25 19:41:43 +0200

    Modified Build.scala

--- project-code/project/Build.scala
+++ project-code/project/Build.scala
@@ -5,14 +5,20 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "csn_mini_blogengine"
-    val appVersion      = "1.0-SNAPSHOT"
-
+    val appVersion      = "0.3.0"
+    
     val appDependencies = Seq(
       "csn_bbcodeparser" % "csn_bbcodeparser_2.9.1" % "1.0-SNAPSHOT",
       "twitter_bootstrap_module" % "twitter_bootstrap_module_2.9.1" % "1.0-SNAPSHOT"
     )
+    
+    object Resolvers {
+  	  val csnRepo = Resolver.ssh("CSN repo", "server.coding-minds.com", 7022, "/var/www/repositories.coding-minds.com/modules/releases/")(Resolver.mavenStylePatterns)
+    }
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      resolvers += "CSN module repository" at "http://repositories.coding-minds.com/modules/releases/"
+      resolvers += "CSN module repository" at "http://repositories.coding-minds.com/modules/releases/",
+      publishMavenStyle := true,
+      publishTo := Some(Resolvers.csnRepo)
     )
 }
